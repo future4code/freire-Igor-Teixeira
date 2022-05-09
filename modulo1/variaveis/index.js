0dd7d254d07b9cd347fdab54e55d4ec35ee50a01
Igorrteixeira <102987283+Igorrteixeira@users.noreply.github.com>
2022-05-09 14:53:37 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,65 @@
+
+
+/*                                                   Exercicio de interpretação
+
+let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a, b)
+
+imprime 10 , 5
+
+
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+imprime 10, 10, 10 
+
+
+
+let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let recebePorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${recebePorDia/horasTrabalhadas} por hora`)*/
+
+
+                                                 // Exercicos de escrita de codigo
+/* 1
+let nome = prompt("qual é o seu nome ?")
+let idade = prompt("Qual é sua idade ?")
+
+console.log("o nome e do tipo ", typeof nome)
+console.log("a idade é do tipo ",typeof idade)
+console.log("Ola",nome," vc tem ",idade,"anos")
+
+
+
+foi impresso string pois nao fiz a conversão para numero no caso da idade*/
+
+
+/* 2
+let roupa = ("vc esta usando roupa azul hoje ")
+let idade = ("vc e maior de idade ? ")
+let carro = ("vc tem carro ? ")
+let resRoupa = ("sim")
+let resIdade = ("sim")
+let resCarro = ("sim")
+console.log(roupa+resRoupa)
+console.log(idade+resIdade)
+console.log(carro+resCarro) */
+
+
+/* 3
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+console.log("O novo valor de a é", a) //25
+console.log("O novo valor de b é", b) //10*/
