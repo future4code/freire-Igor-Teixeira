27a1cb2f4db551b0e7999b7b35e8dc66d111542b
Igorrteixeira <102987283+Igorrteixeira@users.noreply.github.com>
2022-06-10 18:40:43 -0300

    criei-botoes-caixa-de-texto

--- modulo2/projeto-whatslab/whats-lab/src/App.js
+++ modulo2/projeto-whatslab/whats-lab/src/App.js
@@ -8,8 +8,6 @@ class App extends Component {
     return (
       <div>
         <Mensagem/>
-
-        
       </div>
     );
   }
--- modulo2/projeto-whatslab/whats-lab/src/componets/mensagens/mensagens.js
+++ modulo2/projeto-whatslab/whats-lab/src/componets/mensagens/mensagens.js
@@ -1,20 +1,67 @@
 import React from "react";
 import styled from "styled-components"
 import fundo from "../../img/fundo.jpg"
+import enviar from "../../img/enviar2.png"
 
 const ContainerMensagem = styled.div`
+    background: url(${fundo}) no-repeat center;
+    width: 400px;
+    height: 60vh;
+    margin: 0 auto;
+
     display: flex;
-    justify-content: space-around;
-    width: 180vw;
-    height: 90vh;
-    
+    flex-direction: column-reverse;
+     
 `
 const Bloco = styled.div`
     display: flex;
+    align-items: center;
+    gap: 2px;
+    
+    
+`
+const TxtUsuario = styled.input`
+    width:100px;
+    height: 30px;
+    border-radius:30px;
+    border: none;
+`
+const TxtMensagem = styled.input`
+    width: 230px;
+    height: 30px;
+    border-radius:30px;
+    border: none;
+    
 `
 
+const Botao = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #c30e0e ;
+    border-radius:50%;
+    width: 40px;
+    height: 40px;
+    margin:5px;
+    
 
-
+`
+const TextoMensagem = styled.p`
+    background-color: white;
+    border-radius: 10px;
+    color:black;
+    word-wrap:break-word;
+    width:100px;
+`
+const BalaoMensagem = styled.div`
+    margin-left:10px;
+    display: flex;
+    width: 40px;
+    word-wrap:10;
+`
+const ImgEnviar = styled.img`
+    width: 100px;
+`
 class Mensagem extends React.Component{
     state = {
         pessoas:[
@@ -46,24 +93,30 @@ class Mensagem extends React.Component{
     render(){
 
         const componenteMensagem = this.state.pessoas.map((pessoa)=>{
-            return <p>{pessoa.nome} - {pessoa.mensagem}</p> 
+            return <BalaoMensagem>
+                <TextoMensagem>{pessoa.nome} </TextoMensagem>
+                <TextoMensagem>{pessoa.mensagem} </TextoMensagem>
+
+            </BalaoMensagem> 
+            
 
         })
         return(
             <ContainerMensagem>
-                <img src={fundo} alt="" />
+                
                 <Bloco>
-                    <input
+                    <TxtUsuario
                     value={this.state.inputNome}
                     onChange={this.onChangeInputNome}
                     placeholder={"user"}/>
-                    <input
+                    <TxtMensagem
                     value={this.state.inputMensagem}
                     onChange={this.onChangeInputMensagem}
                     placeholder={"Mensagem"} />
-                    <button onClick={this.adicionaMensagem}>Enviar</button>
-                    <div>{componenteMensagem}</div>
+                    <Botao onClick={this.adicionaMensagem}><ImgEnviar src={enviar} alt="" /></Botao>  
                 </Bloco>
+                {componenteMensagem}
+                
             </ContainerMensagem>
 
         )
