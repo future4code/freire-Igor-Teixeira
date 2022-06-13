777e330e48eb83a1eb82fbd6f19f0fadeff08235
Igorrteixeira <102987283+Igorrteixeira@users.noreply.github.com>
2022-06-13 16:42:39 -0300

    projeto-form-condicionais

---
+++ modulo2/rend-condicional-form/form-condicionais/src/components/etapa2.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Etapa2 extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>5 - Qual curso</p>
+                <input type="text" />
+                <p>6 - Qual a unidade de ensino</p>
+                <input type="text" />
+                
+            </div>
+        )
+    }
+}
+export default Etapa2
---
+++ modulo2/rend-condicional-form/form-condicionais/src/components/etapa3.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Etapa3 extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>por que você não terminou um curso de graduação ?</p>
+                <input type="text" />
+                <p>6 - Você fez algum curso complementar?</p>
+                <select name="" id="">
+                    <option value="">Nenhum</option>
+                    <option value="">curso técnico</option>
+                    <option value="">curso inglês</option>
+                    
+                </select>
+            </div>
+        )
+    }
+}
+export default Etapa3
